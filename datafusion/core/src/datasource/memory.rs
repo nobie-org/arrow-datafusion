596d7e5dba41cc02498b428affce54298895a0dc
Tom Walker <tom@nobie.app>
2024-07-25 16:27:31 +0100

    Implement crude stats for mem

--- datafusion/core/src/datasource/memory.rs
+++ datafusion/core/src/datasource/memory.rs
@@ -37,7 +37,8 @@ use crate::physical_planner::create_physical_sort_exprs;
 
 use arrow::datatypes::SchemaRef;
 use arrow::record_batch::RecordBatch;
-use datafusion_common::{not_impl_err, plan_err, Constraints, DFSchema, SchemaExt};
+use datafusion_common::stats::Precision;
+use datafusion_common::{not_impl_err, plan_err, Constraints, DFSchema, SchemaExt, Statistics};
 use datafusion_execution::TaskContext;
 use datafusion_physical_plan::metrics::MetricsSet;
 
@@ -64,11 +65,13 @@ pub struct MemTable {
     /// Optional pre-known sort order(s). Must be `SortExpr`s.
     /// inserting data into this table removes the order
     pub sort_order: Arc<Mutex<Vec<Vec<Expr>>>>,
+    num_rows: usize,
 }
 
 impl MemTable {
     /// Create a new in-memory table from the provided schema and record batches
     pub fn try_new(schema: SchemaRef, partitions: Vec<Vec<RecordBatch>>) -> Result<Self> {
+        let mut num_rows = 0_usize;
         for batches in partitions.iter().flatten() {
             let batches_schema = batches.schema();
             if !schema.contains(&batches_schema) {
@@ -78,6 +81,7 @@ impl MemTable {
                 );
                 return plan_err!("Mismatch between schema and batches");
             }
+            num_rows += batches.num_rows();
         }
 
         Ok(Self {
@@ -89,6 +93,7 @@ impl MemTable {
             constraints: Constraints::empty(),
             column_defaults: HashMap::new(),
             sort_order: Arc::new(Mutex::new(vec![])),
+            num_rows
         })
     }
 
@@ -202,6 +207,12 @@ impl TableProvider for MemTable {
 
     fn table_type(&self) -> TableType {
         TableType::Base
+    }
+
+    fn statistics(&self) -> Option<Statistics> {
+        let mut stats = Statistics::new_unknown(&self.schema);
+        stats.num_rows = Precision::Inexact(self.num_rows);
+        Some(stats)
     }
 
     async fn scan(
